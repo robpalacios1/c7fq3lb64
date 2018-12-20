8bfed372559bc6da537ba074f966bfc9ee54c817
Roberto <robpalacios1@hotmail.com>
2018-12-19 20:16:40 -0500

    Agregando filtros de busqueda

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,7 @@ class ExpensesController < ApplicationController
   def index
     @expenses = Expense.order("date DESC")
     if params[:concept].present?
-      @expenses = @expenses.where("concept ILIKE ?", "%#{params[:concept]}%")
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
     end
     if params[:category_id].present?
       @expenses = @expenses.where("category_id = ?", params[:category_id])
