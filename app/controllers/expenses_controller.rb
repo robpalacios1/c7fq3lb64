ad2d4b8349a34a4466eeabe2654bab3425f8a94f
Roberto <robpalacios1@hotmail.com>
2018-12-29 13:13:46 -0500

    Configurando Devise

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,20 @@
 class ExpensesController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
-    @expenses = current_user.expenses 
+    @expenses = current_user.expenses.order("date DESC")
+    if params[:concept].present?
+      @expenses = @expenses.where("concept LIKE ? ", "%#{params[:concept]}%")
+    end
+    if params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id])
+    end
   end
+
+  def search_params
+    params.permit(:category_id, :concept)
+  end
+
 end
  
---
+++ db/migrate/20181229130000_add_users_to_expensetable.rb
@@ -0,0 +1,5 @@
+class AddUsersToExpensetable < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :expenses, :user, foreign_key: true
+  end
+end
